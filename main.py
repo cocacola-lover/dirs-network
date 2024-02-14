92e9f0953fecd675390f6785f01bf570da1a3e88
Roman Ivashkin <pegas620570@gmail.com>
2024-02-14 15:36:26 +0300

    Create network for containers

--- main.py
+++ main.py
@@ -2,18 +2,38 @@ import docker
 import signal
 import sys
 
+import constants as const
+
 client = docker.from_env()
-container = client.containers.run(
+
+network = client.networks.create(const.DIRS_NETWORK_NAME)
+
+container1 = client.containers.run(
     "cocacola-lover/dirs:0.1", 
     detach=True, 
-    name="dirs-test", 
-    ports={'3334/tcp': 3333}
+    name="dirs-test1", 
+    ports={'3334/tcp': 3333},
+    hostname="testContainer1",
+    network=const.DIRS_NETWORK_NAME,
+    environment=['address=http://testContainer1:3333', 'friends=["http://testContainer2:3333/ask"]']
 )
-print(container)
+
+container2 = client.containers.run(
+    "cocacola-lover/dirs:0.1", 
+    detach=True, 
+    name="dirs-test2", 
+    ports={'3334/tcp': 3334},
+    hostname="testContainer2",
+    network=const.DIRS_NETWORK_NAME,
+)
 
 def signal_handler(sig, frame):
-    container.remove(force=True)
+    print(container1.logs()) 
+    container1.remove(force=True)
+    container2.remove(force=True)
     print('\nContainers have been stopped and removed')
+    network.remove()
+    print('Network has been stopped and removed')
     sys.exit(0)
 
 signal.signal(signal.SIGINT, signal_handler)
