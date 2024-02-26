0f713c2d12a8bf1f6260710302076daae6176b44
Roman Ivashkin <pegas620570@gmail.com>
2024-02-26 16:26:49 +0300

    Extract dirs creation to a func + add log extraction

--- constants.py
+++ constants.py
@@ -1,1 +1,2 @@
 DIRS_NETWORK_NAME = "dirs-network"
+DIRS_IMAGE_NAME = "cocacola-lover/dirs:latest"
--- main.py
+++ main.py
@@ -8,30 +8,33 @@ client = docker.from_env()
 
 network = client.networks.create(const.DIRS_NETWORK_NAME)
 
-container1 = client.containers.run(
-    "cocacola-lover/dirs:0.1", 
-    detach=True, 
-    name="dirs-test1", 
-    ports={'3334/tcp': 3333},
-    hostname="testContainer1",
-    network=const.DIRS_NETWORK_NAME,
-    environment=['address=http://testContainer1:3333', 'friends=["http://testContainer2:3333/ask"]']
+def createContainer(i : int) :
+    return client.containers.run(
+        const.DIRS_IMAGE_NAME, 
+        detach=True, 
+        name=f"dirs-test{i}", 
+        ports={'3334/tcp': 3333 + i},
+        hostname=f"testContainer{i}",
+        network=const.DIRS_NETWORK_NAME,
+        environment=[f'address=http://testContainer{i}:3333', f'friends=["http://testContainer{i+1}:3333/ask"]']
 )
+    
+containers = [createContainer(i) for i in range(2)]
 
-container2 = client.containers.run(
-    "cocacola-lover/dirs:0.1", 
-    detach=True, 
-    name="dirs-test2", 
-    ports={'3334/tcp': 3334},
-    hostname="testContainer2",
-    network=const.DIRS_NETWORK_NAME,
-)
+def extractLogs() :
+    for container in containers :
+        (_, out) = container.exec_run("cat logs.txt") 
+        with open(f"logs/log_{container.name}.txt", "wb") as file:
+            file.write(out)
 
 def signal_handler(sig, frame):
-    print(container1.logs()) 
-    container1.remove(force=True)
-    container2.remove(force=True)
+    
+    extractLogs()
+    
+    for container in containers : container.remove(force=True)
+    
     print('\nContainers have been stopped and removed')
+    
     network.remove()
     print('Network has been stopped and removed')
     sys.exit(0)
